68e430d939479bfa5e5edbcd80f4beef88799a56
jaimebjj <jaime.junior@portalunimed.com.br>
2016-11-28 11:11:27 -0200

    correção para gerar .jar executável

--- creditminer/src/br/com/mj/creditminer/enumerator/CredentialsEnum.java
+++ creditminer/src/br/com/mj/creditminer/enumerator/CredentialsEnum.java
@@ -14,7 +14,9 @@ public enum CredentialsEnum {
 	CREDENTIALS_PARANA_BANCO(Util.getProperty("prop.descricao.parana.banco"), Util.getProperty("prop.login.parana.banco"), Util.getProperty("prop.password.parana.banco")), 
 	CREDENTIALS_PAN(Util.getProperty("prop.descricao.pan"), Util.getProperty("prop.login.pan"), Util.getProperty("prop.password.pan")), 
 	CREDENTIALS_BMG(Util.getProperty("prop.descricao.bmg"), Util.getProperty("prop.login.bmg"), Util.getProperty("prop.password.bmg")), 
-	CREDENTIALS_BOM_SUCESSO(Util.getProperty("prop.descricao.bom.sucesso"), Util.getProperty("prop.login.bom.sucesso"), Util.getProperty("prop.password.bom.sucesso"));
+	CREDENTIALS_BOM_SUCESSO(Util.getProperty("prop.descricao.bom.sucesso"), Util.getProperty("prop.login.bom.sucesso"), Util.getProperty("prop.password.bom.sucesso")),
+	CREDENTIALS_DAYCOVAL(Util.getProperty("prop.descricao.daycoval"), Util.getProperty("prop.login.daycoval"), Util.getProperty("prop.password.daycoval")), 
+	CREDENTIALS_BOM_SAFRA(Util.getProperty("prop.descricao.safra"), Util.getProperty("prop.login.safra"), Util.getProperty("prop.password.safra"));
 
 	private String descricao;
 	private String login;
